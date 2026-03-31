b60e503f3d6aed340e26c0480c99d783130314c4
jrose0215 <jrose0215@gmail.com>
2026-03-31 13:22:29 +0000

    Add FastAPI endpoints and local API screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,38 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics, inference
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model_returns_classifier():
     """
-    # add description for the second test
+    Test that train_model returns a RandomForestClassifier.
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
-    # add description for the third test
+    Test that compute_model_metrics returns correct precision, recall, and F1.
     """
-    # Your code here
-    pass
+    y = np.array([1, 0, 1, 1, 0])
+    preds = np.array([1, 0, 1, 0, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert precision == pytest.approx(1.0)
+    assert recall == pytest.approx(2 / 3)
+    assert 0 <= fbeta <= 1
+
+
+def test_inference_output_shape():
+    """
+    Test that inference returns predictions with the same length as input.
+    """
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    X_test = np.array([[1, 2], [3, 4]])
+    preds = inference(model, X_test)
+    assert len(preds) == len(X_test)
